3e0c2944fc4ba3c322dbb71d88936d3d7e55f525
Gabynna <gabymeraz1978@gmail.com>
2021-08-28 01:14:18 -0500

    avance
    avance en 2 y 3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,17 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  var numeromayor = (x , y);
-  if (x < y)
+ 
+  if (x == y)
   {
-      return numeromayor;
-      
+    return x;
   } 
+  if ( x > y) {
+    return x;
+  }else{
+    return y ;
+    
+  }
 }
 
 function mayoriaDeEdad(edad) {
@@ -54,13 +59,13 @@ function saludo(idioma) {
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
    
-  if (idioma, 'aleman'){
+  if (idioma == 'aleman'){
     return 'Guten Tag!';
 }
-if (idioma, 'mandarin'){
+if (idioma == 'mandarin'){
     return 'Ni Hao!';
 }
-if (idioma, 'ingles'){
+if (idioma == 'ingles'){
     return 'Hello!';
 }
  else {
@@ -131,7 +136,14 @@ function esEntero(numero) {
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
 
- 
+  var entero = Math.floor(numero);
+   
+  if(entero !=numero )
+  {
+      return false;
+  }else{
+      return true;
+  }
  
 
 }
@@ -141,8 +153,22 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-
   
+  if (numero % 3 == 0 && numero % 5 == 0 ){
+    return 'fizzbuzz';
+}
+
+  if(numero % 3 == 0){
+    return 'fizz';
+}
+if (numero % 5 == 0){
+    return 'buzz';
+}
+
+else {
+    return numero;
+}
+
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -152,6 +178,24 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1<0 || num2<0 || num3<0){
+    return 'Hay negativos';
+  }
+
+  if (num1 === 0 || num2 === 0 || num3 === 0){
+    return 'Error';
+  }
+  if (num1>num2 && num1>num3 && num1> 0){
+    return 'Número 1 es mayor y positivo';
+}
+
+   if (num3>num1 &&  num3>num2){
+  num3++
+  return num3;
+}
+
+  return false;
+  
 }
 
 function esPrimo(numero) {
@@ -160,12 +204,30 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-}
+ if (numero === 2){ 
+   return true;}
+  
+ if (numero < 2){
+  return false;
+ }
+  
+ for (var i=2; i<=numero; i++){
+ if (numero % i === 0){
+     return false;
+   } return true;
+ }      
+    }
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+
+  if (valor === true){
+    return 'Soy verdadero';
+}   return 'Soy falso';
+
 
 }
 
@@ -173,20 +235,48 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
+   
+  let array = [];
   
-}
+  for ( let  i=0; i<=10; i++) {
+          array.push(6*i);
+ 
+ }
 
+     return array;
+}
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+
+  var numeroLetra =  numero.toString();
+   
+  if (numeroLetra.length === 3){
+      
+      return true;
+  }
+    
+        return false; 
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-}
+
+  var valor = numero;  
+  var i = 0;
+  
+  do {
+    i=i + 1;
+    valor = valor  + 5; 
+      
+  }while (i < 8); // true o flase
+  
+  return valor;
+  
+  
+  }
 
 
 // No modificar nada debajo de esta línea
